d6af8a483ab9ba782f96476bbe100ec1db8f632a
sdelphine <delph.sebast@gmail.com>
2019-11-29 14:34:55 +0100

    test

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import { Square } from './Square'
+import { Square } from './Square';
 
 export class Board extends React.Component {
     renderSquare(i) {
